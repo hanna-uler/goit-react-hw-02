192054f3f1c7142245c3b08384e9df0061df1abf
hanna-uler <hannauler@gmail.com>
2025-04-24 14:49:30 -0400

    delete global stylesheet, complete css modules

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -3,7 +3,7 @@ import css from "./Notification.module.css"
 export default function Notification() {
     return (
         <div>
-            <p>There is no feedback yet.</p>
+            <p className={css.message}>There is no feedback yet.</p>
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import "modern-normalize";
-import './index.css';
 import App from "../src/components/App/App";
 
 createRoot(document.getElementById('root')).render(
